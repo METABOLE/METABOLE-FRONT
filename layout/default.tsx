0d60ddf80a77ca9ddcfa9f1d2d18ec879eed1ab0
Matteo <matteo.courquin@gmail.com>
2025-03-05 19:48:02 +0100

    :rocket: Add dynamic page title

--- layout/default.tsx
+++ layout/default.tsx
@@ -4,16 +4,23 @@ import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import ScreenLoader from '@/components/ScreenLoader';
 import { useEnvironment } from '@/hooks/useEnvironment';
+import { useLanguage } from '@/providers/language.provider';
 import { AppProvider } from '@/providers/root';
+import { setupTitleChanger } from '@/utils/changeTitle';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Head from 'next/head';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Layout = ({ children }: { children: ReactNode }) => {
   const { isProd } = useEnvironment();
+  const { isFrench } = useLanguage();
+
+  useEffect(() => {
+    setupTitleChanger({ isFrench });
+  }, [isFrench]);
 
   return (
     <AppProvider>
