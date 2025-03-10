e83513c8eed1634ead8960147f54a513270eccf5
Matteo <matteo.courquin@gmail.com>
2025-03-10 15:23:46 +0100

    :rocket: Update my APIS

--- services/config.ts
+++ services/config.ts
@@ -2,5 +2,5 @@ import { config } from '@/config';
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: config.api.baseUrl + '/dev/v1/',
+  baseURL: config.api.baseUrl + '/v1/',
 });
