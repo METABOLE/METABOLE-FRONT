1014b374b835863326c4290974d31b13fbc5e743
Matteo <matteo.courquin@gmail.com>
2025-03-18 08:14:01 +0100

    :bug: Fix 500 on robots.txt & sitemap.xml

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,18 @@
-import { DocumentProps, Html, Main, NextScript } from 'next/document';
+import { DocumentProps, Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document(props: DocumentProps) {
   const isFrench = props.__NEXT_DATA__.query.lang === 'fr';
+  const descriptionEn =
+    'Metabole STUDIO is a creative studio based in Paris, France, founded by Matteo COURQUIN and Jérôme BEZEAU. We create unique web experiences for brands and agencies.';
+  const descriptionFr =
+    'Metabole STUDIO est un studio créatif basé à Paris, France, fondé par Matteo COURQUIN et Jérôme BEZEAU. Nous créons des expériences web uniques pour les marques et les agences.';
 
   return (
     <Html lang={isFrench ? 'fr' : 'en'}>
+      <Head>
+        <meta content={isFrench ? descriptionFr : descriptionEn} name="description" />
+        <link href="/favicon.ico" rel="icon" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
