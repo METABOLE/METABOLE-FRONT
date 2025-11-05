cbc33a00fa3bbf40c092a19f31a84268ad064365
Matteo Courquin <matteo.courquin@gmail.com>
2025-11-05 18:52:14 +0100

    :bug: Fix lock scroll

--- components/layout/ScreenLoader.tsx
+++ components/layout/ScreenLoader.tsx
@@ -1,4 +1,4 @@
-import { useScrollLock } from '@/hooks/useToggleScroll';
+import { useScroll } from '@/hooks/useScroll';
 import { usePerformance } from '@/providers/performance.provider';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
@@ -24,7 +24,7 @@ const ScreenLoader = () => {
   const progressRef = useRef({ value: 0 });
 
   const { contextSafe } = useGSAP();
-  const { lockScroll } = useScrollLock();
+  const { lockScroll } = useScroll();
   const { isLoading } = usePerformance();
 
   const revealAnimation = contextSafe(() => {
@@ -164,7 +164,10 @@ const ScreenLoader = () => {
   }, [isLoading, isRevealed]);
 
   return (
-    <div ref={screenLoaderRef} className="fixed inset-0 z-[950] grid grid-cols-2 grid-rows-2">
+    <div
+      ref={screenLoaderRef}
+      className="fixed inset-0 z-[950] grid grid-cols-2 grid-rows-2 opacity-50"
+    >
       <div ref={maskRef.topLeft} className="h-full w-full origin-left bg-black" />
       <div ref={maskRef.topRight} className="h-full w-full origin-right bg-black" />
       <div ref={maskRef.bottomLeft} className="h-full w-full origin-left bg-black" />
